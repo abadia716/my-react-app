044748486db761a241909417817f1d4844f90270
seuz716 <ceanabad@gmail.com>
2023-02-28 19:03:00 -0500

    Mejora Contenido

---
+++ build/precache-manifest.08296c2e1517dd72841a18fc412604f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d82f5e94a8ba05fa7228",
+    "url": "/static/css/main.9b2c1256.chunk.css"
+  },
+  {
+    "revision": "d82f5e94a8ba05fa7228",
+    "url": "/static/js/main.d82f5e94.chunk.js"
+  },
+  {
+    "revision": "f637ed1cfa609b769d70",
+    "url": "/static/css/1.c3b0c3e8.chunk.css"
+  },
+  {
+    "revision": "f637ed1cfa609b769d70",
+    "url": "/static/js/1.f637ed1c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5b4418798d390578fe677ccde691f9e",
+    "url": "/static/media/Fotico.f5b44187.png"
+  },
+  {
+    "revision": "2051e8f2589e1b9a81577a7a85e51b16",
+    "url": "/static/media/abejaGirasol.2051e8f2.png"
+  },
+  {
+    "revision": "46cc327b44fa190df01dac47e71d415e",
+    "url": "/static/media/Marketing.46cc327b.png"
+  },
+  {
+    "revision": "4617307a3cb00154a3c450e1617e407d",
+    "url": "/static/media/BigData.4617307a.png"
+  },
+  {
+    "revision": "608db6f033547998c22f12e5aae4df3e",
+    "url": "/static/media/seo.608db6f0.jpg"
+  },
+  {
+    "revision": "0d64088f48c36574b0ad263c40677db9",
+    "url": "/index.html"
+  }
+];
